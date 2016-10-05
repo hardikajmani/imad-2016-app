05815171e21e158085a5ebedf4ca524d2e41fd01
hardikajmani <hardikajmani@gmail.com>
2016-10-05 23:27:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/art-one',function(req,res){
+    res.send('Article one ko bulaya  hai');
+});
+
+app.get('/art-two',function(req,res){
+    res.send('Article two ko bulaya hai');
+});
+
+app.get('/art-three',function(req,res){
+    res.send('Article three ko  bulayaq hai');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
