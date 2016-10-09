0f3ae2d16f14879965338ca2e7a20ff9f42b96e5
hardikajmani <hardikajmani@gmail.com>
2016-10-10 00:01:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,60 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var artone={
+    title: 'Article-one',
+    heading: 'Aricle-one',
+    content: `   <p>
+            bla abla jffvbvalvb bla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvb
+        </p>
+        <p>
+            bla abla jffvbvalvb bla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvb
+        </p>
+        <p>
+            bla abla jffvbvalvb bla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvbbla abla jffvbvalvb
+        </p>`
+    
+};
+
+
+function createTemplate(data){ 
+ 
+ var title   =   data.title;
+ var heading =   data.heading;
+ var content =   data.content;
+ var template=
+   ` <html>
+<head>
+    <title>
+        ${title}
+    </title>
+    <meta name="viewport" content="widt=device-width, initial-scale=1"/>
+     <link href="/ui/style.css" rel="stylesheet" />
+</head>
+<body>
+   <div class="cont">
+    <div>
+        <a href="/"> Home </a>
+    </div>
+        <hr/>
+        <h3>
+            This is ${heading} 
+        </h3>
+        ${content}
+    </div>
+</body>
+</html>`;
+return template;
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/art-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'art-one.html'));
+    res.send(createTemplate(artone));
 });
 
 app.get('/art-two',function(req,res){
