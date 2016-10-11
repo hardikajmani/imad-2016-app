34261b53ebdbce299c64fd75535025c9d95076fa
hardikajmani <hardikajmani@gmail.com>
2016-10-11 20:15:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,5 @@ console.log('Loaded!');
 // change text
 var elem = document.getElementById('main');
 
-elem.InnerHTML = " NEw changes BLA bla bla bla ";
+elem.InnerHTML = ' NEw changes BLA bla bla bla ';
 
