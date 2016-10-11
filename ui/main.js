005261144cf0ba970deba6377e1d239c26929958
hardikajmani <hardikajmani@gmail.com>
2016-10-11 20:23:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,8 @@ var elem = document.getElementById('main');
 
 elem.innerHTML = ' NEw changes BLA bla bla bla ';
 
+//To move the picture
+var elem2 = document.getElementById('main2');
+img.onclick = function(){
+    img.style.marginLeft = '100px'
+};
