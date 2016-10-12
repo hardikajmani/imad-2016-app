3443a78496cf0a332ea0e21a70c89e26c4c0b010
hardikajmani <hardikajmani@gmail.com>
2016-10-12 22:17:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,8 @@
 //code to use counter using java script
 var button = document.getElementById('counter');
+
+var counter = 0;
+
 button.onclick=  function(){
      // Accept the response from the counter 
      
