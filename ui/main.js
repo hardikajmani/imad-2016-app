bdd32ecd3216af82808d92161161419124cefed1
hardikajmani <hardikajmani@gmail.com>
2016-10-13 00:34:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,3 +26,22 @@ button.onclick=  function(){
   request.send(null);
      
 };
+
+//submit namae
+var nameInput = document.getElementById('name');
+var name = nameInput.value();
+var submit = document.getElementById('submit_btn');
+
+submit.onclick = function(){
+   //Make the request and sendf to the server
+   
+   //render the request 
+   var names = ['name1', 'name2', 'name3', 'name4'];
+   var list = '';
+   for(var i=0; i<names.length; i++){
+       list += '<li>' + names[i] + '</li>';
+     }
+   var ul = document.getElementById('namelist');
+   ul.innerHTML =  list;
+     
+};
