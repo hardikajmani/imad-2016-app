051eb3559a138ec3a5171156c6f872c61dc35cf9
hardikajmani <hardikajmani@gmail.com>
2016-10-12 21:50:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.get('/', function (req, res) {
 var counter = 0;
 app.get('/counter',function(req,res){
     counter = counter + 1;
-    res.send(counter);
+    res.send(counter.toString());
 })
 
 app.get('/:artName',function(req,res){
