175cd996ffb3d3c0c0053ddd98bc337fd91b6a2e
hardikajmani <hardikajmani@gmail.com>
2016-10-12 21:49:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,6 +72,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter',function(req,res){
+    counter = counter + 1;
+    res.send(counter.toString());
+})
+
 app.get('/:artName',function(req,res){
     var artName = req.params.artName;
     res.send(createTemplate(articles[artName]));
