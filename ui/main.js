712a22f20c449e84b1b1262dd1fd790ebe01cf4e
hardikajmani <hardikajmani@gmail.com>
2016-10-14 00:17:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -33,15 +33,33 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 
 submit.onclick = function(){
-   //Make the request and sendf to the server
    
-   //render the request 
-   var names = ['name1', 'name2', 'name3', 'name4'];
-   var list = '';
-   for(var i=0; i<names.length; i++){
-       list += '<li>' + names[i] + '</li>';
-     }
-   var ul = document.getElementById('namelist');
-   ul.innerHTML =  list;
+
+     // Make the request 
      
+      var request = new XMLHttpRequest();
+     
+     //grab the response 
+     request.onreadystatechange = function(){
+         if(request.readyState === XMLHttpRequest.DONE){
+             if(request.status === 200){
+                //render the request 
+            var names = request.responseText;
+            names = JSON.parse(names);
+            var list = '';
+            for(var i=0; i<names.length; i++){
+                 list += '<li>' + names[i] + '</li>';
+                 }
+            var ul = document.getElementById('namelist');
+            ul.innerHTML =  list;
+
+         }     
+        }
+     };
+        
+  // Making the request
+  request.open('GET','http://hardikajmani.imad.hasura-app.io/submit-name?name' + name,true);
+  request.send(null);
+     
+
 };
